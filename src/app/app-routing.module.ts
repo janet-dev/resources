e6875bf91a4e4063eefd8d355d9873bf213cb549
Janet Dornan <janet.dornan@metrobank.plc.uk>
2024-01-09 10:47:26 +0000

    Remove auth folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 import { ResourcesComponent } from './resources/resources/resources.component';
 import { ResourceDetailComponent } from './resources/resource-detail/resource-detail.component';
 import { AdminResourcesComponent } from './admin-resources/admin-resources.component';
@@ -8,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'resources', component: ResourcesComponent },
   { path: 'resources/detail', component: ResourceDetailComponent },
   { path: 'admin/resources', component: AdminResourcesComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
 import { MaterialDesignModule } from './material-design/material-design.module';
 import { ResourcesComponent } from './resources/resources/resources.component';
 import { ResourceDetailComponent } from './resources/resource-detail/resource-detail.component';
@@ -22,7 +20,6 @@ import { HomeComponent } from './home/home.component';
     MaterialDesignModule,
     BrowserAnimationsModule,
     HomeComponent,
-    LoginComponent,
     ResourcesComponent,
     ResourceDetailComponent
   ],
