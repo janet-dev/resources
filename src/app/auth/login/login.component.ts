fb439a06fad5e2a60cb48f51e9ca463b6cec9e47
Janet Dornan <janet.dornan@metrobank.plc.uk>
2024-01-12 17:21:43 +0000

    Add login form back again

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialDesignModule } from 'src/app/material-design/material-design.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialDesignModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  isLoading: boolean = false;
+  constructor(private router: Router) {}
+
+  login() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 2000);
+  }
 
 }
