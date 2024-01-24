65574ad36b5f2bff7e1a013950642aa8d964509a
Janet Dornan <janet.dornan@metrobank.plc.uk>
2024-01-24 12:31:00 +0000

    Create register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MaterialDesignModule } from 'src/app/material-design/material-design.module';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialDesignModule,
+  ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  isLoading: boolean = false;
+  constructor(private router: Router) {}
+
+  register() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 2000);
+  }
 
 }
