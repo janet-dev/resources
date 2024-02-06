952564e3b65ef1d3d077509f9337c61c05008619
Janet Dornan <janet.dornan@metrobank.plc.uk>
2024-02-06 17:34:08 +0000

    Add Firebase config

---
+++ src/app/firebase.config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZ6RnnhFltjgJy0N2Vl5iMAVRy5N5HJ_4",
+  authDomain: "resources-ng.firebaseapp.com",
+  projectId: "resources-ng",
+  storageBucket: "resources-ng.appspot.com",
+  messagingSenderId: "1087437753477",
+  appId: "1:1087437753477:web:0b637ecd2ed650cf8f2dec"
+};
