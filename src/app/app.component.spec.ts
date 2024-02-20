6da2ac5ca9420423906440f2ad9559f0ce977c5f
Janet Dornan <janet.dornan@metrobank.plc.uk>
2024-02-20 16:52:38 +0000

    Add jasmine tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MaterialDesignModule } from './material-design/material-design.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialDesignModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,24 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'resources'`, () => {
+  it(`should have as title 'Resources'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('resources');
+    expect(app.title).toEqual('Resources');
   });
 
-  it('should render title', () => {
+  it(`should display the branding of 'bōc-hord'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('resources app is running!');
+    expect(compiled.querySelector('mat-toolbar a')?.textContent).toContain('bōc-hord');
   });
+
+  it(`should have initial menu list for Home, Register, Login`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.navList).toEqual(['Home', 'Register', 'Login']);
+  });
+
 });
+// all 4 tests pass
